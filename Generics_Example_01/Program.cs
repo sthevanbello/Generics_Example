764af8a648d962d6a5af6cba8f7d8a73979da0ef
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-16 19:33:29 -0300

    Generic - first example

---
+++ Generics_Example_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Generics_Example_01.Entities;
+
+namespace Generics_Example_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PrintService print = new PrintService();
+
+                Console.Write("How many values? : ");
+                int values = int.Parse(Console.ReadLine());
+
+
+                for (int i = 0; i < values; i++)
+                {
+                    print.AddValue(int.Parse(Console.ReadLine()));
+                }
+
+                print.Print();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }  
+
+
+            Console.ReadKey();
+        }
+    }
+}
